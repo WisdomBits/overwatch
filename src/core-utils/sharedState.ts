2a7149f2b49fbe4c5958f7fddfd803da2af02157
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-06-14 13:11:50 +0530

    Feature : State Immutibility

--- src/core-utils/sharedState.ts
+++ src/core-utils/sharedState.ts
@@ -1,3 +1,6 @@
+import { produce } from 'immer';
+import pubsub from '../core/pubsub';
+
 const stateStore = new Map<string, any>();
 
 export function createSharedState<T>(key: string, initialValue: T) {
@@ -6,9 +9,27 @@ export function createSharedState<T>(key: string, initialValue: T) {
   }
 }
 
+export function setSharedState<T>(key: string, newValue: T) {
+  const prevValue = stateStore.get(key);
+
+    // If both previous and new value are objects, using Immer to deeply update
+    const immutableValue =
+      isPlainObject(prevValue) && isPlainObject(newValue)
+        ? produce(prevValue, (draft: any) => {
+          Object.assign(draft, newValue);
+        })
+        : newValue;
+    stateStore.set(key, immutableValue);
+    pubsub.publish<T>(key, immutableValue as T);
+}
+
 export function getSharedState<T>(key: string): T {
   if (!stateStore.has(key)) {
     throw new Error(`No state found for key: ${key}`);
   }
   return stateStore.get(key);
+}
+
+function isPlainObject(obj: any): obj is Record<string, unknown> { // TypeScript type predicate
+  return obj !== null && typeof obj === "object" && !Array.isArray(obj);
 }
