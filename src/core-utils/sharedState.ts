00d5adce1a45d0b4b9a2e9657442aeea738a900d
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-06-28 14:12:44 +0530

    Persistance API Support

--- src/core-utils/sharedState.ts
+++ src/core-utils/sharedState.ts
@@ -2,19 +2,24 @@ import { produce } from 'immer';
 import pubsub from '../core/pubsub';
 import { runMiddlewares } from './Middleware';
 import { globalStore, ServerStore } from './createServerStore';
+import { persistValue, getPersistedValue, setPersistence } from './persistance';
 
-export function createSharedState<T>(key: string, initialValue: T, store: ServerStore = globalStore) {
+
+export function createSharedState<T>(key: string, initialValue: T, store: ServerStore = globalStore, persist?: "localStorage" | "sessionStorage") {
+  if(persist) setPersistence(key, persist);
+  const persisted = getPersistedValue<T>(key);
   if (!store.getSnapshot().hasOwnProperty(key)) {
-    store.set(key, initialValue);
+    store.set(key,  persisted !== undefined ? persisted : initialValue);
   }
 }
 
 export function batchCreateSharedStates(
   stateObj: Record<string, any>,
-  store?: ServerStore
+  store?: ServerStore,
+  persist?: "localStorage" | "sessionStorage"
 ) {
   Object.entries(stateObj).forEach(([key, value]) => {
-    createSharedState(key, value, store);
+    createSharedState(key, value, store,persist);
   });
 }
 
@@ -24,11 +29,13 @@ export function setSharedState<T>(key: string, newValue: T, store: ServerStore =
   runMiddlewares<T>(key, newValue, (val) => {
     const immutableValue =
       isPlainObject(prevValue) && isPlainObject(val)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         ? produce(prevValue, (draft: any) => {
             Object.assign(draft, val);
           })
         : val;
     store.set(key, immutableValue);
+    persistValue(key, immutableValue);
     pubsub.publish<T>(key, immutableValue);
   });
 }
