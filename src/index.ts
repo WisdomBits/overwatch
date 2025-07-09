e83b2199d95286c2529dd1f5b28d03b874ca2199
Karandeep Singh <soundportrait2530@gmail.com>
2025-07-09 22:45:44 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,3 @@ export {useBroadcast} from "./Hooks/useBroadcast"
 export {useEvent} from "./Hooks/useEvent"
 export {applyMiddleware} from "./core-utils/Middleware"
 export {Hydrated} from "./core-utils/Hydrated";
-
