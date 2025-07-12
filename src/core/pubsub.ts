51263536e124b7cf8031ec019b095db45ed001b1
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-07-12 12:46:30 +0530

    Optimisation : publish API catching Mechanism

--- src/core/pubsub.ts
+++ src/core/pubsub.ts
@@ -25,22 +25,31 @@ const pubsub = {
 
   unsubscribe<T>(event: string, fn: EventCallback<T>) {
     if (this.events[event]) {
-      this.events[event] = this.events[event].filter(sub => sub.fn !== fn);
+      this.events[event] = this.events[event].filter((sub) => sub.fn !== fn);
     }
   },
+  // Optimized: deduplicate by storing only latest value for each event.
+  queuedPublishes: new Map<string, any>(),
 
   publish<T>(event: string, data: T) {
-    if (this.events[event]) {
-      this.events[event].forEach(({ fn, componentName }) => {
-        try {
-          enqueueUpdate(() => fn(data))
-        } catch (err) {
-          console.error(`[${componentName}] Error in "${event}" subscriber:`, err);
-        }
-      });
-    }
-  }
-};
+    this.queuedPublishes.set(event, data);
 
+    enqueueUpdate(() => {
+      const latestData = this.queuedPublishes.get(event);
+      const isAvailable = this.queuedPublishes.delete(event);
+      
+
+      if (this.events[event] && isAvailable) {
+        this.events[event].forEach(({ fn, componentName }) => {
+          try {
+            fn(latestData);
+          } catch (err) {
+            console.error(`[${componentName}] Error in "${event}" subscriber:`, err);
+          }
+        });
+      }
+    });
+  },
+};
 
 export default pubsub;
